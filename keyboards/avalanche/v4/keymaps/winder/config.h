d6d0a356467f3af6b37d2bf46f865dc8b830ed7c
Will Winder <wwinder.unh@gmail.com>
2024-10-29 15:36:27 -0400

    Migrate chocofly and avalanche configs.

---
+++ keyboards/avalanche/v4/keymaps/winder/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Will Winder (@winder)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//#define NO_ACTION_TAPPING // ~1700 bytes
+//#define LAYER_STATE_8BIT // ~130 bytes
---
+++ keyboards/avalanche/v4/keymaps/winder/rules.mk
@@ -0,0 +1,9 @@
+RGBLIGHT_ENABLE = yes
+
+# space saving
+# https://docs.qmk.fm/squeezing_avr
+LTO_ENABLE = yes
+#MOUSEKEY_ENABLE = no # ~1350 bytes
+#SPACE_CADET_ENABLE = no # ~400 bytes
+#GRAVE_ESC_ENABLE = no # ~80 bytes
+#MAGIC_ENABLE = no # ~500 bytes
